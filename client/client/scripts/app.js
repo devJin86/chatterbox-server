9a067e657283a0b2f3ffa1d611435aec8856d275
devJin86 <devJin86@gmail.com>
2016-03-21 20:15:24 -0700

    serve static files

---
+++ client/client/scripts/app.js
@@ -0,0 +1,121 @@
+var app = {};
+ 
+app.friends = [];
+app.rooms = [];
+app.userName;
+app.currentRoom = 'lobby';
+app.url = 'http://127.0.0.1:3000/classes/messages';
+
+app.init = function() {
+  // console.log('refresh');
+  app.fetch(app.url);
+};
+
+app.send = function(message) {
+  $.ajax({
+    url: app.url,
+    type: 'POST',
+    dataType: 'json',
+    data: JSON.stringify(message),
+    contentType: 'application/json; charset=utf-8'
+  });
+};
+
+// app's fetch url - 'https://api.parse.com/1/classes/messages'
+
+app.fetch = function(url) {
+  $.ajax({
+    url: url,
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      var messages = data.results;
+      app.clearMessages();
+      _.each(messages, function(message, i, messages) {
+        app.addMessage(message);
+      });
+    },
+    error: function(data) {
+      console.log('fetch request failed : ', data);
+    }
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+
+app.escapeHTML = function(str) {
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+app.compareFriends = function() {
+  console.log('FRIENDS ARRAY: ', friends);
+};
+
+app.addMessage = function(message) {
+  var username = app.escapeHTML(message['username']);
+  var text = app.escapeHTML(message['text']);
+  var room = app.escapeHTML(message['roomname']);
+  if (app.rooms.indexOf(room) === -1) {
+    app.rooms.push(room);
+    $('select').append('<option>' + room + '</option>');
+  }
+  //console.log(currentRoom, room);
+  if (app.currentRoom === room) {
+    var $message = $('<div class="username chat"></div>');
+    if (app.friends.indexOf(username) > -1) {
+      $message = $message.append('<h2 style="color:blue;">' + username + '</h2>');
+    } else {
+      $message = $message.append('<h2>' + username + '</h2>');
+    }
+
+    $message = $message.append('<p>' + text + '</p>');
+    $('#chats').append($message);
+  } // else { console.log('not current room'); }
+};
+
+app.addRoom = function(roomName) {
+  $('#roomSelect').append('<div>' + roomName + '</div>');
+};
+
+app.addFriend = function() {
+  var name = $(this)[0].firstChild.textContent;
+  if (app.friends.indexOf(name) === -1) {
+    app.friends.push(name);
+  }
+};
+
+//TODO: Make this filter stuff
+app.filterMessages = function(data) {
+  _.each(data.results, function(message, i, list) {
+    app.messages.push(message);
+  });
+};
+
+app.handleSubmit = function(event) {
+  event.preventDefault();
+  var newMessage = {
+    username: $('input[name*="username"]').val(),
+    text: $('input[name*="message"]').val(),
+    roomname: $('input[name*="room"]').val()
+  };
+  app.send(newMessage);
+  return false;
+};
+
+$(document).ready(function() {
+
+  $('#submitFormJezzebelle').submit(app.handleSubmit);
+
+  $('select').on('change', function() {
+    app.currentRoom = $(this).val();
+  });
+
+});
+
+$(document).delegate('.username', 'click', app.addFriend);
+
+setInterval(app.init, 1000);
--- server/request-handler.js
+++ server/request-handler.js
@@ -11,43 +11,68 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+// var data = fs.readFileSync('input.txt');
+// console.log("Synchronous read: " + data.toString());
+
+// console.log("Program Ended");
 
 var url = require('url');
-var qs = require('querystring');
+var fs = require('fs');
+var path = require('path');
+
 var messages = {results: []};
 
 var requestHandler = function(request, response) {
 
-  var statusCode;
+  var statusCode, data, file;
   var headers = defaultCorsHeaders;
-  headers['Content-Type'] = 'JSON';
-
+  
+  var pathTo = '../client';
+  pathTo = pathTo + request.url;
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-  if (request.url !== '/classes/messages') {
-    statusCode = 404;
-    response.writeHead(statusCode, headers);
-    response.end();
-  } else if (request.method === 'GET') {
+
+  if (request.url === '/') {
+    headers['Content-Type'] = 'html';
     statusCode = 200;
+    data = fs.readFileSync('../client/index.html');
     response.writeHead(statusCode, headers);
-
-    response.end(JSON.stringify(messages));
-
-  } else if (request.method === 'POST') {
-    var body = '';
-    request.on('data', function(chunk) {
-      body += chunk;
-    });
-    request.on('end', function() {
-      var newMessage = JSON.parse(body);
-      messages.results.push(newMessage);
-
-      statusCode = 201;
+    response.end(data);
+  } else if (request.url !== '/classes/messages') {
+    if (true) {
+      console.log(pathTo);
+      statusCode = 200;
+      data = fs.readFileSync(pathTo);
+      response.writeHead(statusCode, headers);
+      response.end(data);
+    } else {
+      statusCode = 404;
       response.writeHead(statusCode, headers);
       response.end();
-      // console.log(response);
-    });
-    
+    }
+  } else {
+    headers['Content-Type'] = 'JSON';
+    if (request.method === 'GET') {
+      statusCode = 200;
+      response.writeHead(statusCode, headers);
+
+      response.end(JSON.stringify(messages));
+
+    } else if (request.method === 'POST') {
+      var body = '';
+      request.on('data', function(chunk) {
+        body += chunk;
+      });
+      request.on('end', function() {
+        var newMessage = JSON.parse(body);
+        messages.results.push(newMessage);
+
+        statusCode = 201;
+        response.writeHead(statusCode, headers);
+        response.end();
+        // console.log(response);
+      });
+      
+    }
   }
   
 };
